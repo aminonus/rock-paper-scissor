de91185bad695b0ebe716d3047e67bd3bb8c986a
aminonus <amivo.13@gmail.com>
2022-10-26 20:13:19 +0400

    Updated script.js and added the main game mechanics with number of rounds and checking if inputs are valid

--- script.js
+++ script.js
@@ -2,15 +2,19 @@
 let scorePlayer = 0;
 let scoreComputer = 0;
 let numberRounds;
+let playerChoice;
+let pcChoice;
 
 //Main Sequence of the game
 setNumberRounds();
-
-
-
-
-
-
+ for (let i=1; i<= numberRounds; i++){
+    console.log(`Round number: ${i}`);
+    checkPlayerChoice();
+    console.log(`You chose: ${playerChoice}`);
+    checkPcChoice();
+    console.log(`PC chose: ${pcChoice}`);
+    checkResults(playerChoice, pcChoice);
+ }
 
 
 
@@ -26,4 +30,35 @@ function setNumberRounds() {
         setNumberRounds();
     }
 
+}
+
+//Check the player choice if it is valid
+function checkPlayerChoice(){
+    let choice = prompt(`Please choose [Rock] [Paper] [Scissor]`)
+    toString(choice);
+    let choiceTrim = choice.trim();
+    let choiceUP = choiceTrim.toUpperCase();
+    if ((choiceUP === "ROCK") || (choiceUP === "PAPER") || (choiceUP === "SCISSOR")){
+        let choiceLoPart = choiceUP.slice(1);
+        let choiceLO = choiceLoPart.toLowerCase();
+        let choiceDisplay = choiceUP[0]+choiceLO;
+        playerChoice = choiceDisplay;
+        return playerChoice;
+    }
+    else {
+        checkPlayerChoice();
+    }
+}
+
+//Randomize the PC choice
+function checkPcChoice() {
+    let choices = ["Rock", "Paper", "Scissor"];
+    let choiceRandom = Math.floor(Math.random() * 3);
+    pcChoice = choices[choiceRandom];
+    return pcChoice;
+}
+
+//Check the results
+function checkResults(player, pc){
+    
 }
